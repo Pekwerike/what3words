0cdccbac1189d4c0f62d810c789129687a376ace
Prosper Ekwerike <pekwerike@gmail.com>
2021-12-12 15:52:48 +0100

    code refactor

--- lib/src/main/java/com/pekwerike/lib/components/recyclerview/adapter/What3WordsSuggestionPickerAdapterDiffUtil.kt
+++ lib/src/main/java/com/pekwerike/lib/components/recyclerview/adapter/What3WordsSuggestionPickerAdapterDiffUtil.kt
@@ -4,11 +4,11 @@ import androidx.recyclerview.widget.DiffUtil
 import com.pekwerike.lib.domain.Suggestion
 
 internal object What3WordsSuggestionPickerAdapterDiffUtil : DiffUtil.ItemCallback<Suggestion>() {
-    override fun areItemsTheSame(oldItem: Suggestion, newItem: Suggestion): Boolean {
-        return oldItem.words == newItem.words
-    }
+    override fun areItemsTheSame(oldItem: Suggestion, newItem: Suggestion): Boolean =
+        oldItem.words == newItem.words
 
-    override fun areContentsTheSame(oldItem: Suggestion, newItem: Suggestion): Boolean {
-        return oldItem == newItem
-    }
+
+    override fun areContentsTheSame(oldItem: Suggestion, newItem: Suggestion): Boolean =
+        oldItem == newItem
+
 }
